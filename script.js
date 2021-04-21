c3ea8b353505ba14841930bb0934df80c716f869
Erika Hernández <74832999+erika-her-o@users.noreply.github.com>
2021-04-21 15:14:44 -0400

    js and img

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('contact').onsubmit = event => event.preventDefault();
+document.getElementById('send').onclick = () => alert('Bienvenid@, ya eres miembro. Recibirás noticias, descuentos y mas...');
+
+
+
